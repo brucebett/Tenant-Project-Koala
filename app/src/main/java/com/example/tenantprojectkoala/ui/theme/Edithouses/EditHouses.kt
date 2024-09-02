932b8da9596de0f3e81ada6aed98259be3d9497b
brucebett <bett6448@gmail.com>
2024-09-02 23:51:54 +0300

    2SEPT9 WIP

--- app/src/main/java/com/example/tenantprojectkoala/ui/theme/Edithouses/EditHouses.kt
+++ app/src/main/java/com/example/tenantprojectkoala/ui/theme/Edithouses/EditHouses.kt
@@ -4,6 +4,23 @@ import android.net.Uri
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
@@ -11,11 +28,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberImagePainter
 import com.example.tenantprojectkoala.R
+import com.example.tenantprojectkoala.data.HouseViewModel
 import com.example.tenantprojectkoala.models.House
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -67,4 +95,81 @@ fun EditHouse(navController: NavController, id: String) {
         currentDataRef.addValueEventListener(listener)
         onDispose { currentDataRef.removeEventListener(listener) }
     }
+
+    Column (modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .padding(10.dp)
+        .fillMaxWidth()
+        .background(Color.LightGray)
+    ) {
+        Text(text = "EDIT HOUSE",
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+                .background(Color.Blue)
+        )
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "ALL HOUSES")
+            }
+            Button(onClick = { 
+                val houseRepository = HouseViewModel(navController, context)
+                houseRepository.editHouse(
+                    context = context,
+                    navController = navController,
+                    filepath = imageUri.value ?: Uri.EMPTY,
+                    price = price,
+                    size = size,
+                    location = location,
+                    phonenumber = phonenumber,
+                    description = description,
+                    id = id,
+                    currentImageUrl = existingImageUrl
+                    
+                )
+            }) { Text(text = "EDIT")}
+        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(shape = CircleShape,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(180.dp)
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(180.dp)
+                        .clickable { launcher.launch("image/*") },
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Text(text = "Edit Picture Here")
+        }
+        OutlinedTextField(modifier = Modifier
+            .wrapContentWidth()
+            .align(Alignment.CenterHorizontally),
+            label = { Text(text = "Enter House Size")},
+            placeholder = { Text(text = "Please Enter House Size")},
+            value = size, 
+            onValueChange = { newName -> size = newName })
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun EditHousePreview() {
+    EditHouse(rememberNavController(), id = "")
 }
